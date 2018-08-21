17e602a2268b537c32d4de4f8ade79c3caa56a0e
Botond Sipos <Botond.Sipos@nanoporetech.com>
2018-08-21 14:18:56 +0100

    Added some comments.

--- transcript_abundance/pool.go
+++ transcript_abundance/pool.go
@@ -41,6 +41,7 @@ func (p *TranscriptPool) GetCompatibility(recs []*PafRecord) {
 	fullLengthMinDistance := 20
 	minReadLength := 0
 	threshold := 0.95
+	alignmentThreshold := 0.5
 
 	//Determine best match:
 	readLength := recs[0].QueryLength
@@ -57,11 +58,14 @@ func (p *TranscriptPool) GetCompatibility(recs []*PafRecord) {
 		}
 	}
 
+	// Skip read if fails filters:
 	fractionAligned := float64(bestMatchAlignLen) / float64(readLength)
-	if fractionAligned < 0.5 || readLength < minReadLength {
+	if fractionAligned < alignmentThreshold || readLength < minReadLength {
 		return // FIXME
 	}
 
+	// Is equivalent hit if score is high enough
+	// an full length status matches:
 	isEquHit := func(r *PafRecord) bool {
 		f := float64(r.NrMatches) / float64(bestNumMatches)
 		l := r.TargetStart < fullLengthMinDistance
@@ -76,6 +80,7 @@ func (p *TranscriptPool) GetCompatibility(recs []*PafRecord) {
 		}
 	}
 
+	// "Distribute" the hits equally between the targets:
 	for _, rec := range recs {
 		if isEquHit(rec) {
 			p.Compat[rec.QueryName] = append(p.Compat[rec.QueryName], CompRecord{rec.TargetName, 1.0 / float64(numHits)})
